ab463735f60cb7929283720015e5e07c4ab45339
vandau1181994 <200263yw@yse-c.net>
2021-12-01 12:02:08 +0900

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+//routes.js をよみこむ
+const routes = require('./routes')
+
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -7,31 +10,15 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
-app.post('/auth', (req, res) => {
-    const login_name = req.body.login_name
-    const password = req.body.password
+//ejs をつかう
+app.set('view engine', 'ejs')
 
-    let message = 'ログインできません'
-    if (login_name == process.env.login_name
-        && password == process.env.PASSWORD){
-            message = 'ログインしました'
+//routes をつかう
+app.use(routes)
 
-        }
-    res.send(message)
-})
-
-app.get('/', (req, res) =>{
-    res.send('Hello YSE !!!')
-})
-app.get('/profile', (req, res) =>{
-    res.send('this is profile page!!!')
-})
-
-
-app.listen(port, host, () => {
-
+　app.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
 })
